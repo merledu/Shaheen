19c1bc1a5bf998133734c361462369972eac8a08
Hadir Khan <hadirkhan10@gmail.com>
2020-08-29 12:14:39 +0500

    Created a SubReg with ready-write, read-only and write-only configuration

---
+++ src/main/scala/shaheen/registers/SubReg.scala
@@ -0,0 +1,43 @@
+package shaheen.registers
+import chisel3._
+
+
+class SubReg(DW: Int = 32, SWACCESS: String = "RW")(RESVAL: UInt = 0.U(DW.W)) extends Module {
+  val io = IO(new Bundle {
+    // From software
+    val we = Input(Bool())  // Indicates the software wants to write in the register
+    val wd = Input(UInt(DW.W))  // The data software wants to write
+    // From hardware
+    val de = Input(Bool())  // Indicates the peripheral logic wants to write in the register
+    val d = Input(UInt(DW.W)) // The data peripheral logic wants to write
+    // To hardware and software reads
+    val qe = Output(Bool()) // Indicates the peripheral logic that software has written in register
+    val q = Output(UInt(DW.W))  // Output of data in the register for peripheral logic
+    val qs = Output(UInt(DW.W)) // Output of data in the register for software read
+  })
+
+  val wr_en = Wire(Bool())
+  val wr_data = Wire(UInt(DW.W))
+
+  val qe_reg = RegInit(false.B)
+  val q_reg = RegInit(RESVAL)
+
+  if((SWACCESS == "RW") || (SWACCESS == "WO")) {
+    wr_en := io.we || io.de
+    wr_data := Mux(io.we, io.wd, io.d)
+  } else if(SWACCESS == "RO") {
+    wr_en := io.de
+    wr_data := io.d
+  } else {
+    wr_en := io.de
+    wr_data := io.d
+  }
+
+  qe_reg := io.we
+  when(wr_en) {
+    q_reg := wr_data
+  }
+
+  io.qs := q_reg
+  io.q := q_reg
+}

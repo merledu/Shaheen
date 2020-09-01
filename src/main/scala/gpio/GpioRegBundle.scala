953d4e453241bed9dd4ae7d015d92b1a595b22de
Hadir Khan <hadirkhan10@gmail.com>
2020-09-01 17:34:47 +0500

    Created bundles for register interfaces of gpio

---
+++ src/main/scala/gpio/GpioRegBundle.scala
@@ -0,0 +1,149 @@
+package gpio
+import chisel3._
+/**
+ * This file contains all the bundles that will be required by the peripheral
+ */
+
+class GPIOReg2Hw(DW: Int = 32) extends Bundle {
+  val intr_state = new Bundle {
+    val q = Output(UInt(DW.W))
+  }
+
+  val intr_enable = new Bundle {
+    val q = Output(UInt(DW.W))
+  }
+
+  val intr_test = new Bundle {
+    val q = Output(UInt(DW.W))
+    val qe = Output(Bool())
+  }
+
+  val direct_out = new Bundle {
+    val q = Output(UInt(DW.W))
+    val qe = Output(Bool())
+  }
+
+  val masked_out_lower = new Bundle {
+    val data = new Bundle {
+      val q = Output(UInt((DW/2).W))  // half word data
+      val qe = Output(Bool())
+    }
+    val mask = new Bundle {
+      val q = Output(UInt((DW/2).W))  // half word mask
+      val qe = Output(Bool())
+    }
+  }
+
+  val masked_out_upper = new Bundle {
+    val data = new Bundle {
+      val q = Output(UInt((DW/2).W))
+      val qe = Output(Bool())
+    }
+    val mask = new Bundle {
+      val q = Output(UInt((DW/2).W))
+      val qe = Output(Bool())
+    }
+  }
+
+  val direct_oe = new Bundle {
+    val q = Output(UInt(DW.W))
+    val qe = Output(Bool())
+  }
+
+  val masked_oe_lower = new Bundle {
+    val data = new Bundle {
+      val q = Output(UInt((DW/2).W))
+      val qe = Output(Bool())
+    }
+    val mask = new Bundle {
+      val q = Output(UInt((DW/2).W))
+      val qe = Output(Bool())
+    }
+  }
+
+  val masked_oe_upper = new Bundle {
+    val data = new Bundle {
+      val q = Output(UInt((DW/2).W))
+      val qe = Output(Bool())
+    }
+    val mask = new Bundle {
+      val q = Output(UInt((DW/2).W))
+      val qe = Output(Bool())
+    }
+  }
+
+  val intr_ctrl_en_rising = new Bundle {
+    val q = Output(UInt(DW.W))
+  }
+
+  val intr_ctrl_en_falling = new Bundle {
+    val q = Output(UInt(DW.W))
+  }
+
+  val intr_ctrl_en_lvlHigh = new Bundle {
+    val q = Output(UInt(DW.W))
+  }
+
+  val intr_ctrl_en_lvlLow = new Bundle {
+    val q = Output(UInt(DW.W))
+  }
+
+}
+
+class GPIOHw2Reg(DW: Int = 32) extends Bundle {
+  val intr_state = new Bundle {
+    val d = Input(UInt(DW.W))
+    val de = Input(Bool())
+  }
+
+  val data_in = new Bundle {
+    val d = Input(UInt(DW.W))
+    val de = Input(Bool())
+  }
+
+  val direct_out = new Bundle {
+    val d = Input(UInt(DW.W))
+  }
+
+  val masked_out_lower = new Bundle {
+    val data = new Bundle {
+      val d = Input(UInt((DW/2).W))
+    }
+    val mask = new Bundle {
+      val d = Input(UInt((DW/2).W))
+    }
+  }
+
+  val masked_out_upper = new Bundle {
+    val data = new Bundle {
+      val d = Input(UInt((DW/2).W))
+    }
+    val mask = new Bundle {
+      val d = Input(UInt((DW/2).W))
+    }
+  }
+
+  val direct_oe = new Bundle {
+    val d = Input(UInt(DW.W))
+  }
+
+  val masked_oe_lower = new Bundle {
+    val data = new Bundle {
+      val d = Input(UInt((DW/2).W))
+    }
+    val mask = new Bundle {
+      val d = Input(UInt((DW/2).W))
+    }
+  }
+
+  val masked_oe_upper = new Bundle {
+    val data = new Bundle {
+      val d = Input(UInt((DW/2).W))
+    }
+    val mask = new Bundle {
+      val d = Input(UInt((DW/2).W))
+    }
+  }
+}
+
+

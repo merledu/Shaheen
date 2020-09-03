9e830cf53e46c959370e8422860cc334bafccf0a
Hadir Khan <hadirkhan10@gmail.com>
2020-09-03 15:37:00 +0500

    Created a testbench for gpio reg top

---
+++ src/test/scala/gpio/GpioTest.scala
@@ -0,0 +1,15 @@
+package gpio
+import org.scalatest._
+import chiseltest._
+import chisel3._
+import tilelink.TLConfiguration
+
+class GpioTest extends FlatSpec with ChiselScalatestTester with Matchers {
+  implicit val conf = TLConfiguration()
+  behavior of "Gpio"
+  it should "just connect properly and pass test" in {
+    test(new GpioRegTop) {c =>
+      c.clock.step(10)
+    }
+  }
+}

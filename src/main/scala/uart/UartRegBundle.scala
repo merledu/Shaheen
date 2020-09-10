4c91a84824032cb5ab06def640cf980c9b9a0442
Hadir Khan <hadirkhan10@gmail.com>
2020-09-10 10:06:55 +0500

    Added the hardware to register bundle

--- src/main/scala/uart/UartRegBundle.scala
+++ src/main/scala/uart/UartRegBundle.scala
@@ -155,3 +155,66 @@ class UartReg2HW extends Bundle {
     }
   }
 }
+
+class UartHw2Reg extends Bundle {
+  val intr_state = new Bundle {
+    val tx_empty = new Bundle {
+      val d = Input(Bool())
+      val de = Input(Bool())
+    }
+    val rx_overflow = new Bundle {
+      val d = Input(Bool())
+      val de = Input(Bool())
+    }
+    val rx_frame_err = new Bundle {
+      val d = Input(Bool())
+      val de = Input(Bool())
+    }
+    val rx_break_err = new Bundle {
+      val d = Input(Bool())
+      val de = Input(Bool())
+    }
+    val rx_timeout = new Bundle {
+      val d = Input(Bool())
+      val de = Input(Bool())
+    }
+    val rx_parity_err = new Bundle {
+      val d = Input(Bool())
+      val de = Input(Bool())
+    }
+  }
+
+  val status = new Bundle {
+    val txfull = new Bundle {
+      val d = Input(Bool())
+    }
+    val rxfull = new Bundle {
+      val d = Input(Bool())
+    }
+    val txempty = new Bundle {
+      val d = Input(Bool())
+    }
+    val txidle = new Bundle {
+      val d = Input(Bool())
+    }
+    val rxidle = new Bundle {
+      val d = Input(Bool())
+    }
+    val rxempty = new Bundle {
+      val d = Input(Bool())
+    }
+  }
+
+  val rdata = new Bundle {
+    val d = Input(UInt(8.W))
+  }
+
+  val fifo_status = new Bundle {
+    val txlvl = new Bundle {
+      val d = Input(UInt(6.W))
+    }
+    val rxlvl = new Bundle {
+      val d = Input(UInt(6.W))
+    }
+  }
+}

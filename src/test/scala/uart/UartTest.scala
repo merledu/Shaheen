2203be39aeb8158bf6d02d40822f88b527e56670
Hadir Khan <hadirkhan10@gmail.com>
2020-09-14 11:45:02 +0500

    Modified the testbench for checking the connections of TX module

--- src/test/scala/uart/UartTest.scala
+++ src/test/scala/uart/UartTest.scala
@@ -5,12 +5,13 @@ import chiseltest._
 import tilelink.TLConfiguration
 
 class UartTest extends FlatSpec with ChiselScalatestTester with Matchers {
-  implicit val conf = TLConfiguration()
-  behavior of "uart register top"
+  //implicit val conf = TLConfiguration()
+  behavior of "uart tx module"
 
   it should "just connect without errors" in {
-    test(new UartRegTop) {c =>
+    test(new UartTx()) {c =>
       c.clock.step(10)
     }
   }
+
 }

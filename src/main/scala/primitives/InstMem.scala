44a15e030dc3b0d27621dbb2816eda2c3b7d13ea
Hadir Khan <hadirkhan10@gmail.com>
2020-10-02 18:29:45 +0500

    Changed the program file path

--- src/main/scala/primitives/InstMem.scala
+++ src/main/scala/primitives/InstMem.scala
@@ -13,7 +13,7 @@ class InstMem extends Module {
   val mem = SyncReadMem(16383, UInt(32.W))
  // val addr = Wire(UInt(32.W))
   
-  loadMemoryFromFile(mem,"/Users/mbp/Desktop/instructions.txt")
+  loadMemoryFromFile(mem,"/home/merl/Desktop/mem.txt")
 
   io.instr_rdata_o := mem.read(io.instr_addr_i)
   

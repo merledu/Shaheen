2d3ac24ec3a178057c92f56439f156ad09ad550c
Hadir Khan <hadirkhan10@gmail.com>
2020-10-04 23:26:26 +0500

    Modified the top to use the TL SRAM adapter rather than the register adapter

--- src/main/scala/shaheen_soc/ShaheenTop.scala
+++ src/main/scala/shaheen_soc/ShaheenTop.scala
@@ -1,7 +1,7 @@
 package shaheen_soc
 import chisel3._
 import _root_.core.Core
-import merl.uit.tilelink.{TLConfiguration, TL_H2D, TL_HostAdapter, TL_RegAdapter}
+import merl.uit.tilelink.{TLConfiguration, TL_H2D, TL_HostAdapter, TL_RegAdapter, TL_SramAdapter}
 import primitives.{DataMem, InstMem}
 
 class ShaheenTop(implicit val conf: TLConfiguration) extends Module {
@@ -13,8 +13,8 @@ class ShaheenTop(implicit val conf: TLConfiguration) extends Module {
   val dccm = Module(new DataMem())
   val core_iccm_tl_host = Module(new TL_HostAdapter())
   val core_dccm_tl_host = Module(new TL_HostAdapter())
-  val iccm_tl_device = Module(new TL_RegAdapter(14, 32, forSRAM = true.B)())
-  val dccm_tl_device = Module(new TL_RegAdapter(regAw = 14, regDw = 32, forSRAM = true.B)())
+  val iccm_tl_device = Module(new TL_SramAdapter(sramAw = 14, sramDw = 32, forFetch = true.B))
+  val dccm_tl_device = Module(new TL_SramAdapter(sramAw = 14, sramDw = 32))
 
   /** |||||||||||| CORE -> TL_HOST ADAPTER -> TL_DEVICE ADAPTER -> ICCM |||||||||||| */
   /** |||||||||||| ICCM -> TL_DEVICE ADAPTER -> TL_HOST ADAPTER -> CORE |||||||||||| */
@@ -27,10 +27,8 @@ class ShaheenTop(implicit val conf: TLConfiguration) extends Module {
   iccm_tl_device.io.tl_i <> core_iccm_tl_host.io.tl_o
   core_iccm_tl_host.io.tl_i <> iccm_tl_device.io.tl_o
 
-  iccm.io.instr_req_i := iccm_tl_device.io.re_o
   iccm.io.instr_addr_i := iccm_tl_device.io.addr_o >> 2
   iccm_tl_device.io.rdata_i := iccm.io.instr_rdata_o
-  iccm_tl_device.io.error_i := false.B  // may get error from 1:N socket with address mishandling
 
   core.io.instr_rdata_i := core_iccm_tl_host.io.rdata_o
   core.io.instr_gnt_i := core_iccm_tl_host.io.gnt_o
@@ -46,13 +44,12 @@ class ShaheenTop(implicit val conf: TLConfiguration) extends Module {
 
   dccm_tl_device.io.tl_i <> core_dccm_tl_host.io.tl_o
   core_dccm_tl_host.io.tl_i <> dccm_tl_device.io.tl_o
-  dccm_tl_device.io.error_i := false.B
   dccm_tl_device.io.rdata_i := dccm.io.rdata_o
 
   dccm.io.addr_i := dccm_tl_device.io.addr_o >> 2
   dccm.io.wdata_i := dccm_tl_device.io.wdata_o
   dccm.io.we_i := dccm_tl_device.io.we_o
-  dccm.io.mask_i := dccm_tl_device.io.be_o
+  dccm.io.mask_i := dccm_tl_device.io.wmask_o
 
   core.io.data_gnt_i := core_dccm_tl_host.io.gnt_o
   core.io.data_rvalid_i := core_dccm_tl_host.io.valid_o

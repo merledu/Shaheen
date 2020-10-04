63ac365a015eec84f346252883e1341303a68389
Hadir Khan <hadirkhan10@gmail.com>
2020-10-04 23:25:06 +0500

    Changed the program path

--- src/main/scala/primitives/InstMem.scala
+++ src/main/scala/primitives/InstMem.scala
@@ -5,15 +5,14 @@ import chisel3.util.experimental.loadMemoryFromFile
 
 class InstMem extends Module {
   val io = IO(new Bundle{
-    val instr_req_i = Input(Bool())
     val instr_addr_i = Input(UInt(14.W))
     val instr_rdata_o = Output(UInt(32.W))
   })
 
-  val mem = SyncReadMem(16383, UInt(32.W))
+  val mem = SyncReadMem(16384, UInt(32.W))
  // val addr = Wire(UInt(32.W))
   
-  loadMemoryFromFile(mem,"/home/merl/Desktop/mem.txt")
+  loadMemoryFromFile(mem,"/Users/mbp/Desktop/instructions.txt")
 
   io.instr_rdata_o := mem.read(io.instr_addr_i)
   

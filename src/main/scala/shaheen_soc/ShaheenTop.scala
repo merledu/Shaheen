38f63fd508a999fc014bd8dd34b3b4f93d3f62fc
Hadir Khan <hadirkhan10@gmail.com>
2020-10-08 15:13:41 +0500

    Modified the top file according to the masked synchronous memory DCCM

--- src/main/scala/shaheen_soc/ShaheenTop.scala
+++ src/main/scala/shaheen_soc/ShaheenTop.scala
@@ -1,12 +1,13 @@
 package shaheen_soc
 import chisel3._
+import chisel3.util.Cat
 import _root_.core.Core
 import merl.uit.tilelink.{TLConfiguration, TL_H2D, TL_HostAdapter, TL_RegAdapter, TL_SramAdapter}
 import primitives.{DataMem, InstMem}
 
 class ShaheenTop(implicit val conf: TLConfiguration) extends Module {
   val io = IO(new Bundle {
-    val result = Output(SInt(32.W))  // just a dummy output
+    val result = Output(SInt(32.W))  // output for testing on fpga
   })
   val core = Module(new Core())
   val iccm = Module(new InstMem())
@@ -40,16 +41,20 @@ class ShaheenTop(implicit val conf: TLConfiguration) extends Module {
   core_dccm_tl_host.io.addr_i := core.io.data_addr_o.asUInt()
   core_dccm_tl_host.io.we_i := core.io.data_we_o
   core_dccm_tl_host.io.wdata_i := core.io.data_wdata_o.asUInt()
-  core_dccm_tl_host.io.be_i := core.io.data_be_o
+  core_dccm_tl_host.io.be_i := Cat(core.io.data_be_o(3),core.io.data_be_o(2),core.io.data_be_o(1),core.io.data_be_o(0))
 
   dccm_tl_device.io.tl_i <> core_dccm_tl_host.io.tl_o
   core_dccm_tl_host.io.tl_i <> dccm_tl_device.io.tl_o
-  dccm_tl_device.io.rdata_i := dccm.io.rdata_o
+  dccm_tl_device.io.rdata_i := Cat(dccm.io.rdata_o(3),dccm.io.rdata_o(2),dccm.io.rdata_o(1),dccm.io.rdata_o(0))
 
   dccm.io.addr_i := dccm_tl_device.io.addr_o >> 2
-  dccm.io.wdata_i := dccm_tl_device.io.wdata_o
+
+  dccm.io.wdata_i(0) := dccm_tl_device.io.wdata_o(7,0)
+  dccm.io.wdata_i(1) := dccm_tl_device.io.wdata_o(15,8)
+  dccm.io.wdata_i(2) := dccm_tl_device.io.wdata_o(23,16)
+  dccm.io.wdata_i(3) := dccm_tl_device.io.wdata_o(31,24)
   dccm.io.we_i := dccm_tl_device.io.we_o
-  dccm.io.mask_i := dccm_tl_device.io.wmask_o
+  dccm.io.wmask_i := dccm_tl_device.io.wmask_o
 
   core.io.data_gnt_i := core_dccm_tl_host.io.gnt_o
   core.io.data_rvalid_i := core_dccm_tl_host.io.valid_o

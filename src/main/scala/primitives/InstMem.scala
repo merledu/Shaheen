569b4293eabfdf6cccfc9e093636fadf47571e1e
Hadir Khan <hadirkhan10@gmail.com>
2020-10-14 19:04:33 +0500

    Modified the iccm to have write port for uart interface

--- src/main/scala/primitives/InstMem.scala
+++ src/main/scala/primitives/InstMem.scala
@@ -5,16 +5,28 @@ import chisel3.util.experimental.loadMemoryFromFile
 
 class InstMem extends Module {
   val io = IO(new Bundle{
-    val instr_addr_i = Input(UInt(14.W))
-    val instr_rdata_o = Output(UInt(32.W))
+//    val instr_addr_i = Input(UInt(14.W))
+//    val instr_rdata_o = Output(UInt(32.W))
+    val csb_i = Input(Bool())   // active low chip select
+    val we_i = Input(Bool())    // active low write enable
+    val addr_i = Input(UInt(14.W))
+    val wdata_i = Input(UInt(32.W))
+    val rdata_o = Output(UInt(32.W))
   })
 
   val mem = SyncReadMem(16384, UInt(32.W))
- // val addr = Wire(UInt(32.W))
   
-  loadMemoryFromFile(mem,"/home/merl/Desktop/mem.txt")
+//  loadMemoryFromFile(mem,"/home/merl/Desktop/mem.txt")
+//
+//  io.instr_rdata_o := mem.read(io.instr_addr_i)
 
-  io.instr_rdata_o := mem.read(io.instr_addr_i)
-  
+  when(!io.csb_i && !io.we_i) {
+    mem.write(io.addr_i, io.wdata_i)
+    io.rdata_o := DontCare
+  } .elsewhen(!io.csb_i && io.we_i) {
+    io.rdata_o := mem.read(io.addr_i)
+  } .otherwise {
+    io.rdata_o := DontCare
+  }
   
 }

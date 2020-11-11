eac0a6d69958b6919775c155a1ac45c8d56059ce
Hadir Khan <hadirkhan10@gmail.com>
2020-11-11 13:30:51 +0500

    Changed the memory rows for 1KB storage space

--- src/main/scala/primitives/DataMem.scala
+++ src/main/scala/primitives/DataMem.scala
@@ -5,13 +5,13 @@ class DataMem extends Module {
   val io = IO(new Bundle {
     val csb_i = Input(Bool())   // active low chip select
     val we_i = Input(Bool())    // active low write enable
-    val addr_i = Input(UInt(14.W))
+    val addr_i = Input(UInt(8.W))
     val wdata_i = Input(Vec(4, UInt(8.W)))
     val wmask_i = Input(Vec(4, Bool()))
     val rdata_o = Output(Vec(4, UInt(8.W)))
   })
 
-  val mem = SyncReadMem(16384, Vec(4, UInt(8.W)))
+  val mem = SyncReadMem(256, Vec(4, UInt(8.W)))
   when(!io.csb_i && !io.we_i) {
     mem.write(io.addr_i, io.wdata_i, io.wmask_i)
     io.rdata_o := DontCare

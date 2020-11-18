1199a9ddd9055755e12dde87a120b2a4b356a310
Hadir Khan <hadirkhan10@gmail.com>
2020-11-18 16:57:17 +0500

    Changed the dccm memory interface according to the dffram

--- src/main/scala/primitives/DataMem.scala
+++ src/main/scala/primitives/DataMem.scala
@@ -4,22 +4,31 @@ import chisel3._
 /** TODO: Have to make changes here to support DFFRAM memory interfaces */
 class DataMem extends Module {
   val io = IO(new Bundle {
-    val csb_i = Input(Bool())   // active low chip select
-    val we_i = Input(Bool())    // active low write enable
+    val en_i = Input(Bool())   // active high memory enable
+    val we_i = Input(Vec(4, Bool()))    // active high write enable which acts as a mask as well
     val addr_i = Input(UInt(8.W))
     val wdata_i = Input(Vec(4, UInt(8.W)))
-    val wmask_i = Input(Vec(4, Bool()))
+    //val wmask_i = Input(Vec(4, Bool()))
     val rdata_o = Output(Vec(4, UInt(8.W)))
   })
 
   val mem = SyncReadMem(256, Vec(4, UInt(8.W)))
-  when(!io.csb_i && !io.we_i) {
-    mem.write(io.addr_i, io.wdata_i, io.wmask_i)
-    io.rdata_o := DontCare
-  } .elsewhen(!io.csb_i && io.we_i) {
+  when(io.en_i) {
     io.rdata_o := mem.read(io.addr_i)
+    when(io.we_i.contains(true.B).asBool()) {
+      mem.write(io.addr_i, io.wdata_i, io.we_i)
+    }
   } .otherwise {
     io.rdata_o := DontCare
   }
+
+//  when(io.en_i && io.we_i) {
+//    mem.write(io.addr_i, io.wdata_i, io.wmask_i)
+//    io.rdata_o := DontCare
+//  } .elsewhen(!io.en_i && io.we_i) {
+//    io.rdata_o := mem.read(io.addr_i)
+//  } .otherwise {
+//    io.rdata_o := DontCare
+//  }
 
 }
